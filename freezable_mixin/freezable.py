f7f09981f05279a969cab762084e57732e9b2315
Ederic Oytas <edericoytas@gmail.com>
2022-06-25 21:04:55 -0700

    add constructors and methods

--- freezable_mixin/freezable.py
+++ freezable_mixin/freezable.py
@@ -1,8 +1,33 @@
 
 
 class _FreezableData:
-    pass
+    """Holds the data for the Freezable mixin."""
+
+    def __init__(self):
+        
+        self.frozen: bool = False
+        # True if the Freezable object is frozen; False otherwise.
 
 
 class Freezable:
-    pass
+    """Freezable mixin class."""
+
+    def __init__(self):
+        """Initialize this Freezable object."""
+        
+        self.__data: _FreezableData = _FreezableData()
+        # Where the Freezable data is stored
+    
+    def _freeze(self) -> None:
+        """Freeze this object. All methods/operations that would mutate this
+        object are disabled."""
+        self.__data.frozen = True
+
+    def _unfreeze(self) -> None:
+        """Unfreeze this object. All methods/operations that would mutate this
+        object are re-enabled."""
+        self.__data.frozen = False
+
+    def _is_frozen(self) -> bool:
+        """Check if this object is frozen."""
+        return self.__data.frozen
