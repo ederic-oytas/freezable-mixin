c6e80a70efa8baec957f773076ea388790bb0bb5
Ederic Oytas <edericoytas@gmail.com>
2022-07-12 19:47:15 -0700

    Refactor: Replace frozen status with flag

--- freezable/freezable.py
+++ freezable/freezable.py
@@ -8,14 +8,6 @@ _F = TypeVar('_F', bound=Callable)
 # the function signature can be preserved.
 
 
-class _FrozenStatus:
-    """Data class that stores the frozen status of a Freezable object."""
-    
-    def __init__(self):
-        """Create a _FrozenStatus object with a status of unfrozen."""
-        self.frozen: bool = False
-
-
 class FrozenError(RuntimeError):
     """Raised when an operation that could mutate a Freezable object
     is used when that object is frozen."""
@@ -24,14 +16,7 @@ class FrozenError(RuntimeError):
 class Freezable:
     """Freezable mixin class."""
     
-    #
-    # Frozen Status
-    #
-    @property
-    def _Freezable__status(self) -> _FrozenStatus:
-        """True if this object is frozen; False otherwise. This property is
-        considered to be private; its name is pre-mangled."""
-        return self.__dict__.setdefault('_Freezable__status', _FrozenStatus())
+    _Freezable__frozen_flag: bool = False
     
     #
     # Freezing Methods
@@ -40,16 +25,16 @@ class Freezable:
     def freeze(self) -> None:
         """Freeze this object. All methods/operations that could mutate this
         object are disabled."""
-        self._Freezable__status.frozen = True
+        object.__setattr__(self, '_Freezable__frozen_flag', True)
 
     def unfreeze(self) -> None:
         """Unfreeze this object. All methods/operations that could mutate this
         object are re-enabled."""
-        self._Freezable__status.frozen = False
+        object.__setattr__(self, '_Freezable__frozen_flag', False)
 
     def is_frozen(self) -> bool:
         """Check if this object is frozen."""
-        return self._Freezable__status.frozen
+        return self._Freezable__frozen_flag
         
     #
     # Special methods
@@ -57,13 +42,13 @@ class Freezable:
     
     def __setattr__(self, __name: str, __value: Any) -> None:
         """Set an attribute. Raises a FrozenError if this object is frozen."""
-        if self._Freezable__status.frozen:
+        if self.is_frozen():
             raise FrozenError('cannot set attributes while object is frozen')
         object.__setattr__(self, __name, __value)
     
     def __delattr__(self, __name: str) -> None:
         """Delete an attribute. Raises a FrozenError is this object is frozen."""
-        if self._Freezable__status.frozen:
+        if self.is_frozen():
             raise FrozenError('cannot set attributes while object is frozen')
         object.__delattr__(self, __name)        
 
@@ -76,7 +61,7 @@ def disabled_when_frozen(method: _F) -> _F:
     @wraps(method)
     def wrapped(*args, **kwargs):
         self = args[0]
-        if self._Freezable__status.frozen:
+        if self.is_frozen():
             if hasattr(method, '__name__'):
                 raise FrozenError("cannot call method '%s' while object is "
                                   "frozen" % method.__name__)
